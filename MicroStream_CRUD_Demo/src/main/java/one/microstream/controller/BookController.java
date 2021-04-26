aafe9a9df59ff45f34051c4ec6aae6693fb133a4
Kümmel Christian <c.kuemmel@microstream.one>
2021-04-26 11:47:17 +0200

    Add "createSingleBook" service

--- MicroStream_CRUD_Demo/src/main/java/one/microstream/controller/BookController.java
+++ MicroStream_CRUD_Demo/src/main/java/one/microstream/controller/BookController.java
@@ -1,10 +1,13 @@
 package one.microstream.controller;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import one.microstream.domain.Author;
 import one.microstream.domain.Book;
 import one.microstream.storage.DB;
 import one.microstream.utils.MockupUtils;
@@ -21,6 +24,17 @@ public class BookController
 		// Enter your code here
 		
 		return HttpResponse.ok("Books successfully created!");
+	}
+	
+	@Get("/createSingle")
+	public HttpResponse<?> createSingleBook()
+	{
+		Author author = new Author("100", "John", "Doe", "j.doe@example.com", "Male");
+		Book book = new Book("123456789", "Single Book", LocalDate.now(), new BigDecimal(13.32), author);
+		
+		// Enter your code here
+		
+		return HttpResponse.ok("Book successfully created!");
 	}
 	
 	@Get
