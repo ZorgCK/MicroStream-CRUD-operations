59dcce657a30923b7cd350cae411eeb61a777b7b
Kümmel Christian <c.kuemmel@microstream.one>
2023-05-16 14:38:56 +0200

    impl done

--- MicroStream_CRUD_Demo/src/main/java/one/microstream/storage/DataRoot.java
+++ MicroStream_CRUD_Demo/src/main/java/one/microstream/storage/DataRoot.java
@@ -1,14 +1,22 @@
 package one.microstream.storage;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import one.microstream.domain.Book;
 
 
 public class DataRoot
 {
-	private final List<Book> books = new ArrayList<Book>();
+	private final List<Book>		books			= new ArrayList<Book>();
+	private final List<Book>		syncListBooks	= Collections.synchronizedList(new ArrayList<Book>());
+	private final Set<Book>			syncSetBooks	= Collections.synchronizedSet(new HashSet<Book>());
+	private final Map<String, Book>	syncBooks		= Collections.synchronizedMap(new HashMap<String, Book>());
 	
 	public List<Book> getBooks()
 	{
